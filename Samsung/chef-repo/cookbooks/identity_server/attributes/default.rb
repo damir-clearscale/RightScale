344dab63e3c5d3846b95a73e69c9ec474ef3342b
Damir Islamov <damir@clearscale.net>
2013-10-12 15:23:21 +0700

    fix plugin name. SAMSUNGRS-43

--- Samsung/chef-repo/cookbooks/identity_server/attributes/default.rb
+++ Samsung/chef-repo/cookbooks/identity_server/attributes/default.rb
@@ -12,4 +12,4 @@ override[:identity_server][:deps] = ["default-jdk", "default-jre", "ant", "unzip
 
 override[:identity_server][:carbon][:repository] = "http://dist.wso2.org/p2/carbon/releases/4.2.0"
 override[:identity_server][:carbon][:features] = ["org.wso2.carbon.cassandra-jdbc-1.1.1.server_4.2.0"]
-override[:identity_server][:carbon][:plugins] = ["cassandra-jdbc_1.1.1.wso2v1.jar"]
+override[:identity_server][:carbon][:plugins] = ["cassandra-jdbc_1.1.1.wso2v1"]
