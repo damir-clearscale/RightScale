903b8ff4a467cfa12b7002ec344929a8f1ccedcf
Damir Islamov <damir@clearscale.net>
2013-10-19 19:04:09 +0700

    fixes in a template. SAMSUNGRS-43

--- Samsung/chef-repo/cookbooks/identity_server/attributes/default.rb
+++ Samsung/chef-repo/cookbooks/identity_server/attributes/default.rb
@@ -12,3 +12,5 @@ override[:identity_server][:deps] = ["default-jdk", "default-jre", "ant", "unzip
 override[:identity_server][:carbon][:features] = ["org.wso2.carbon.cassandra-jdbc-1.1.1.server_4.2.0"]
 override[:identity_server][:carbon][:plugins] = ["cassandra-jdbc_1.1.1.wso2v1"]
 override[:identity_server][:carbon][:libs] = ["mysql-connector-java-5.1.16"]
+
+default[:identity_server][:mysql] = "" # fix NPE
